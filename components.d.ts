852b3acc110801412be3e140efae4eb35096475d
dimarionss <dimarionss@gmail.com>
2024-05-17 17:19:37 +0300

    refactoring

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    AppBar: typeof import('./src/components/AppBar.vue')['default']
     Pagination: typeof import('./src/components/Pagination.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
